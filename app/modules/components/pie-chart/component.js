afc6bada2055fd8729e1974439380435aec7ffc4
wodelu <1058744843@qq.com>
2018-07-10 20:45:13 +0800

    饼状图组件加入ember项目，数据显示和文字块提示未完成

--- app/modules/components/pie-chart/component.js
+++ app/modules/components/pie-chart/component.js
@@ -1,4 +1,102 @@
 import Component from '@ember/component';
+import d3 from 'd3';
 
 export default Component.extend({
+
+    init() {
+        this._super(...arguments)
+        // window.console.info(d3)
+        // this.dataValue = [1,4,6,4];
+        this.legendValue= ['aa','bb','cc','dd'];
+        this.colorValue = ['red','lightblue','green','orange'];
+    },
+    didInsertElement() {
+        this._super(...arguments);
+        var width = 960,
+        height = 500,
+        radius = Math.min(width, height) / 2 - 10;
+
+// var data = [0.3,0.2,0.2,0.3];
+// var data_show = [1,7,8,6];
+// var arr = ['aaa','111','222','333','444']
+// window.console.info(data_show);
+// var color = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728",];
+        let data = this.get('dataValue');
+        let arr = this.get('legendValue');
+        let color = this.get('colorValue');
+var arc = d3.arc()
+    .outerRadius(radius);
+
+var pie = d3.pie();
+
+var svg = d3.select("body").append("svg")
+    .datum(data)
+    // .datum(data_show)
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+var arcs = svg.selectAll("g.arc")
+    .data(pie)
+    // .data_show(pie)
+  .enter().append("g")
+    .attr("class", "arc");
+    var legend = d3.select("body").append("svg")
+          .attr("class", "legend")
+          .attr("width", 120)
+          .attr("height", 300)
+        .selectAll("g")
+          .data(arr.slice(1).reverse())
+        .enter().append("g")
+          .attr("transform", function(d, i) { return "translate(0," + i * 40 + ")"; });
+
+      legend.append("circle")
+          .attr("cx", 15)
+          .attr("cy", 15)
+          .attr("r", 10)
+          .style("fill", "red");
+
+      legend.append("text")
+          .attr("x", 24)
+          .attr("y", 9)
+          .attr("dy", ".35em")
+          .text(function(d) { return d; });
+
+arcs.append("path")
+    .attr("fill", function(d, i) { return color[i]; })
+  .transition()
+    // .ease("bounce")
+    .duration(2000)
+    .attrTween("d", tweenPie)
+  .transition()
+    // .ease("elastic")
+    .delay(function(d, i) { return 2000 + i * 50; })
+    .duration(750)
+    .attrTween("d", tweenDonut);
+
+function tweenPie(b) {
+  b.innerRadius = 0;
+  var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
+  return function(t) { return arc(i(t)); };
+}
+
+function tweenDonut(b) {
+  b.innerRadius = radius * .6;
+  var i = d3.interpolate({innerRadius: 0}, b);
+  return function(t) { return arc(i(t)); };
+}
+
+
+// var arr = [{age:"1",pp:"22"},{age:"2",pp:"22"},{age:"3",pp:"22"},{age:"4",pp:"22"},{age:"5",pp:"22"},{age:"6",pp:"22"},{age:"7",pp:"22"} ];
+//   console.info(arr);
+//   arr.forEach(function(d) {
+//     var c = labelArc.centroid(d);
+// //     console.log(c);
+// //     console.log(d);
+//     context.fillText(d.age, c[0], c[1]);
+//     console.info(d.age)
+//   });
+
+    },
 });
